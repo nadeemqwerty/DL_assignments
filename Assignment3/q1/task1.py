8ace840897f97c02f191a1841ec5a655b9a439fa
Mohd Nadeem <nadeemqwerty@gmail.com>
2019-04-20 21:04:22 +0530

    task1 train

--- Assignment3/q1/task1.py
+++ Assignment3/q1/task1.py
@@ -2,13 +2,11 @@ from __future__ import print_function
 import numpy as np
 import os
 
-batch_size = 32
-epochs = 12
-shape = 128,128
 import keras
 from keras.applications import ResNet50
 from keras.models import Model,Sequential
-from keras.layers import Lambda, Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization, GaussianNoise, Input, Dropout, concatenate
+from keras.layers import Lambda, Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization
+from keras.layers import GlobalAveragePooling2D, GaussianNoise, Input, Dropout, concatenate
 from keras import optimizers
 from keras.preprocessing.image import img_to_array
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
@@ -16,26 +14,36 @@ import matplotlib.pyplot as plt
 import keras.backend as K
 from keras.backend import tf as ktf
 
-data_dir = "../data/Q1/task1"
+batch_size = 32
+epochs = 16
+shape = (128,128,3)
 
-prev_model = ResNet50(weights='imagenet',include_top=False, input_shape=(128,128,3))
+data_dir = "data/"
+
+images = np.load(data_dir+"images.npy")
+boxes = np.load(data_dir+"boxes.npy")
+labels = np.load(data_dir+"labels.npy")
+
+
+model = ResNet50(weights='imagenet',include_top=False, input_shape=(128,128,3))
+
 
 inp = model.input
 x = model.output
-opt2 = Dense(10, activation = 'relu', name = 'fc_2')(opt)
-reg = Model(input_tensor, x)
-optimizer = keras.optimizers.RMSprop()
-reg.compile(loss = 'logcosh', optimizer = optimizer, metrics = ['accuracy'])
+x = GlobalAveragePooling2D()(x)
 
-images = np.load("")
-box = np.load("")
+classification = Dense(256, activation = 'relu')(x)
+classification = Dense(3, activation = 'softmax',name='classification')(classification)
 
-datagen = ImageDataGenerator(
-    rotation_range=5,
-    width_shift_range=0.1,
-    shear_range = 0.1,
-    # zoom_range = 0.1,
-    height_shift_range=0.1)
+reg = Dense(256, activation = 'relu')(x)
+reg = Dense(4, name = 'reg')(reg)
 
+model = Model(inp, [classification,reg])
+model.summary()
 
-datagen.fit(x_train)
+loss = ['categorical_crossentropy','logcosh']
+loss_weights = [0.1, 2.0]
+
+model.compile(optimizer = 'adam', loss = loss,loss_weights = loss_weights, metrics = ['accuracy'])
+
+history = model.fit(images, [labels,boxes], epochs=epochs, batch_size=batch_size, shuffle=True,validation_split = 0.1,verbose=1)
